13ef853bb766fa133d59eda3663e8b3d79d5868b
carsten <carsten.cibura@nexgo.de>
2017-07-22 19:17:37 +0200

    Travis could not find function test_that ... I added statement library(testthat) to test file, and @importFrom tags to roxygen2 comments;

--- tests/test_make_filename.R
+++ tests/test_make_filename.R
@@ -1,7 +1,12 @@
+library(testthat)
+
 #' Super minimal test suite
 #'
 #' Tests that the function \code{make_filename} returns the expected string for
 #' specific input value, namely for \code{2014}.
+#' @importFrom testthat test_that
+#' @importFrom testthat expect_that
+#' @importFrom testthat equals
 test_that("myTest", {
   filename <- make_filename(2014)
   expect_that(filename, equals("accident_2014.csv.bz2"))
